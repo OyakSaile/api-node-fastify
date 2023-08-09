6308e918adb10b5f7eaeaa477c96547801c9d6be
Kayo Elias <contatodevkayo@gmail.com>
2023-08-08 21:05:48 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+
+const app = fastify();
+
+app.listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
+
+app.get('/hello', () => {
+  return 'hello-worldaaa'
+})
