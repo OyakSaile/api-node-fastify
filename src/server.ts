3a9177dbab48141fc106690fa17c978d545cc689
Kayo Elias <contatodevkayo@gmail.com>
2023-08-20 14:18:58 -0300

    deploy fix

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server Running!')
 })
